3c5fb3a5a1e1504ba8b79c55add381323df1a049
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-01-27 16:05:24 +0530

    array rotation method 1:w

---
+++ src/main/java/com/balaji/dsa/datastructures/array/arrayRotation.java
@@ -0,0 +1,31 @@
+package com.balaji.dsa.datastructures.array;
+public class arrayRotation{
+	public static void main(String[] args) {
+		int[] arr;
+		int size;
+		int noOfIndexsToRotate;
+	}
+
+	public arrayRotation(int[] arr, int size, int noOfIndexsToRotate){
+		this.arr = arr;
+		this.size = size;
+		this.noOfIndexsToRotate = noOfIndexsToRotate;
+	}
+	public method1(){
+		int[] temp;
+		int[] arr1 = arr;
+		for (int i = 0; i < noOfIndexsToRotate; i++) {
+			temp[i] = arr[i];
+			// stored all the values to be rotated
+		}
+		for (int i = 0; i < (size - noOfIndexsToRotate); i++) {
+			arr1[i] = arr[i+noOfIndexsToRotate];
+			// updating copied values with next values
+		}
+		for (int i = 0; i < noOfIndexsToRotate ; i++) {
+			arr1[size-noOfIndexsToRotate+i] = temp[i];
+			// updating the end values with the copied values
+		}
+	}
+	
+}
